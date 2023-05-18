fc6f328c89ebe4bffc1a7078f4264fc72f68c09c
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-05-18 14:56:37 +0300

    Add project files.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var currentCulture = Thread.CurrentThread.CurrentCulture;
+
+        try
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-us");
+            string shortDateString = DateTime.Now.ToShortDateString();
+            // Do something with shortDateString...
+            if (DateTime.Now.Day <= 10)
+            {
+                Console.WriteLine("Buna dimineata!");
+            }
+            else if (DateTime.Now.Day <= 20)
+            {
+                Console.WriteLine("Buna ziua!");
+            }
+            else
+            {
+                Console.WriteLine("Buna seara!");
+            }
+        }
+        finally
+        {
+            Thread.CurrentThread.CurrentCulture = currentCulture;
+        }
+    }
+}
